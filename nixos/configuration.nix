764d02eadf14ad1f4d716484adba758be3536f4c
vlad <Yellow_Vortext@pm.me>
2025-11-30 11:04:20 +0500

    Create configuration.nix

---
+++ nixos/configuration.nix
@@ -0,0 +1,28 @@
+{ pkgs, stateVersion, hostname, ... }:
+
+{
+  imports = [
+    ./hardware-configuration.nix
+    ./packages.nix
+    ./modules/default.nix
+  ];
+
+  networking.hostName = "nixos";
+
+  services.printing.enable = true;
+
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  nixpkgs.config.allowUnfree = true;
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  services.flatpak.enable = true;
+
+  i18n.defaultLocale = "ru_RU.UTF-8";
+
+  system.stateVersion = "25.05";
+}
