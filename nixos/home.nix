ae3376d60aa8dd6b8d88b11bbcb63b2c0b95a0cc
vlad <Yellow_Vortext@pm.me>
2025-12-04 22:35:45 +0500

    Update home.nix

--- nixos/home.nix
+++ nixos/home.nix
@@ -3,7 +3,7 @@
 {
   home.username = "vlad";
   home.homeDirectory = "/home/vlad";
-  home.stateVersion = "25.05";
+  home.stateVersion = "25.11";
   programs.git.enable = true;
   programs.bash = {
     enable = true;
