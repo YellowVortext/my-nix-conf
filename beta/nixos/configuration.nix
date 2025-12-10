99deab9efcfff637c813cad0433d091adda410a2
vlad <Yellow_Vortext@pm.me>
2025-12-10 16:40:53 +0500

    Add files via upload

---
+++ beta/nixos/configuration.nix
@@ -0,0 +1,40 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [
+      ./hardware-configuration.nix
+      ./packages.nix
+      ./modules/default.nix
+    ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  networking.hostName = "eva";
+
+  networking.networkmanager.enable = true;
+
+  time.timeZone = "Asia/Tashkent";
+
+  i18n.defaultLocale = "ru_RU.UTF-8";
+
+  users.users.vlad = {
+    isNormalUser = true;
+    description = "vlad";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+      vscode
+    ];
+  };
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  nixpkgs.config.allowUnfree = true;
+
+  services.flatpak.enable = true;
+
+  system.stateVersion = "25.11";
+}
